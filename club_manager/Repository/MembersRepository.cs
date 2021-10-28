ca5c0e2519f93be14e7caeaa38ccb8a2d14f0efb
Dennis Bingham <dennis.bingham@stukent.com>
2021-10-28 14:24:54 -0600

    fixed MembersRepo

--- club_manager/Repository/MembersRepository.cs
+++ club_manager/Repository/MembersRepository.cs
@@ -7,7 +7,7 @@ namespace club_manager.Repository
 {
     public class MembersRepository : IMembers
     {
-        private List<Member> listMembers = new List<Member>
+        private readonly List<Member> _listMembers = new List<Member>
         {
             new Member{ MemberId = 1, FirstName = "John", LastName = "Doe", Address = "1690 International Way", City = "Idaho Falls", ZipCode = "83402", PhoneNumber = "(505) 798-6324"},
             new Member{ MemberId = 2, FirstName = "Jane", LastName = "Doe", Address = "1690 International Way", City = "Idaho Falls", ZipCode = "83402", PhoneNumber = "(505) 798-6354"},
@@ -15,7 +15,7 @@ namespace club_manager.Repository
             new Member{ MemberId = 4, FirstName = "Rita", LastName = "Dung", Address = "1732 Wallibee Way", City = "Sydney", ZipCode = "12356", PhoneNumber = "(987) 456-9984"}
         };
 
-        public List<Member> GetAllMembers() => listMembers;
-        public Member GetMember(int id) => listMembers.FirstOrDefault(m => m.MemberId.Equals(id));
+        public List<Member> GetAllMembers() => _listMembers;
+        public Member GetMember(int id) => _listMembers.FirstOrDefault(m => m.MemberId.Equals(id));
     }
 }
